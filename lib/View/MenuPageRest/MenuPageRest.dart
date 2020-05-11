23ac8d5a506f4e60f59c55de9f7eb9f818d694b6
Carlos Eduardo Terra <yudden2505@gmail.com>
2020-05-10 23:42:48 -0300

    pagina do cardapio restaurante com comidas e bebidas

---
+++ lib/View/MenuPageRest/MenuPageRest.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:CapiCardapio/View/Ultis.dart';
+import './comidas_rest.dart' as first;
+import './bebidas_rest.dart' as second;
+
+void main() => runApp(MaterialApp(
+  home: MyTabs(),
+));
+
+class MyTabs extends StatefulWidget {
+  @override
+  MyTabsState createState() => MyTabsState();
+}
+
+class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
+
+  TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(vsync: this, length: 2);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text ("Cardápio do Restaurante"),
+          centerTitle: true,
+          backgroundColor: Color.fromRGBO(173, 40, 49, 1),
+          bottom: TabBar(
+              indicatorColor: Colors.white,
+              controller: controller,
+              tabs: <Tab>[
+                Tab(icon: Text("COMIDAS")),
+                Tab(icon: Text("BEBIDAS")),
+              ]
+          )
+      ),
+      body: TabBarView(
+        controller: controller,
+        children: <Widget> [
+          first.Comidas(),
+          second.Bebidas()
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/View/MenuPageRest/comidas_rest.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:CapiCardapio/View/Ultis.dart';
+
+class Comidas extends StatefulWidget {
+  @override
+  _ComidasState createState() => _ComidasState();
+}
+
+class _ComidasState extends State<Comidas> {
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        padding: EdgeInsets.all(17.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 24.0,
+          crossAxisSpacing: 17.0,
+          childAspectRatio: 0.7894736842105263,
+        ),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return InkWell(
+            child: Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 1.125,
+                    child: Image.asset(
+                      'images/Prato_Executivo.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Expanded(
+                      child: Container(
+                          color: Color.fromRGBO(173, 40, 49, 1),
+                          padding: EdgeInsets.all(5),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "Nome do Produto",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Row(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      "R\$ ",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    Text(
+                                      "00.00                    ",
+                                      //virgula ou ponto pra divisao decimal?
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w300),
+                                    ),
+                                    GestureDetector(
+                                      child: Icon(
+                                        Icons.favorite,
+                                        color: Colors.white,
+                                        size: 25,
+                                      ),
+                                    )
+                                  ]),
+                            ],
+                          )))
+                ],
+              ),
+            ),
+          );
+        });
+  }
+}
